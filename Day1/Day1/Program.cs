b6244137e1768dc2bd7a60ada64a51091f134db6
Amr-Mohamed <amr1236661@gmail.com>
2024-01-19 20:58:03 +0200

    first simple calculator and complete number using c#

--- Day1/Day1/Program.cs
+++ Day1/Day1/Program.cs
@@ -4,9 +4,58 @@ namespace Day1
 {
     internal class Program
     {
-       static void Main(string[] args)
+        static void Main(string[] args)
         {
+            Console.WriteLine("For Calculator enter 1\n For Complete Numbers enter 2");
+            string progChoice=Console.ReadLine();
+            switch(progChoice)
+            {
+                case "1":
+                    Calculator();
+                    break;
+                case "2":
+                    CompleteNumbers();
+                    break;
+            }
+        }
+
+        static void CompleteNumbers()
+        {
+            Console.WriteLine("Please enter your number! \n");
+            string compNum=Console.ReadLine();
+            if(int.TryParse(compNum,out int num))
+            {
+                NumProg(num);
+            }
+            else
+            {
+                Console.WriteLine("Please enter a valid number");
+            }
             
+        }
+        static void NumProg(int num)
+        {
+            int progress = 0;
+            for(int i = 0; i < num;i++)
+            {
+                if (progress < num)
+                {
+                progress += i;
+                    Console.WriteLine($"Your number progress is:{container}");
+                }
+                else
+                {
+                    break;
+                }
+            Console.WriteLine(i);
+            }
+        }
+
+
+        static void Calculator()
+        {
+
+        
             Console.WriteLine("Hello human \n Please enter your first number \n");
            string firstNum = Console.ReadLine();
 
